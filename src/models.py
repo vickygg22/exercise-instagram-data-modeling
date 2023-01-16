4c9e9a7f07b0d1291b2a68de648f4d8b2ba2edde
Victoria <victoria1998castellar@hotmail.com>
2023-01-16 19:13:14 +0000

    final version

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,64 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(40))
+    password = Column(String(40))
+    first_name = Column(String(30))
+    last_name = Column(String(40))
+    email = Column(String(50))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_type = Column(String(30))
+    post_details = Column(String(300))
+    date = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to = relationship(User)
+
+class Reel(Base):
+    __tablename__ = 'reel'
+    id = Column(Integer, primary_key=True)
+    reel_details = Column(String(300))
+    date = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    users_reel = relationship(User)
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to = relationship(User)
+    user_from_id = Column(Integer, ForeignKey('follower.id'))
+    user_from = relationship(Follower)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    reel_id = Column(Integer, ForeignKey('reel.id'))
+    reel = relationship(Reel)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(300))
+    date = Column(DateTime)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to = relationship(User)
+    user_from_id = Column(Integer, ForeignKey('follower.id'))
+    user_from = relationship(Follower)
+    reel_id = Column(Integer, ForeignKey('reel.id'))
+    reel = relationship(Reel)
 
     def to_dict(self):
         return {}
